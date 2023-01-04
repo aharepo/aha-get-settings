e29c79447b52ce5386e7c994e4259a6be51e2722
Le Thanh Hieu <hieult@dgroup.co>
2023-01-04 12:51:34 +0700

    update sign jwt

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,8 @@
 export const INTERNAL_SERVICES = {
   AHA_SEARCH: {
     name: "AHA_SEARCH",
-    allowedSettings: ['redis_url']
+    allowedSettings: ["redis_url"]
   },
 };
+
+export const ACCESS_TOKEN_EXPIRED_TIME = 30000;
